91d5fc6cdf908edb963eadba93896186458088ad
roga.lin@gmail.com <roga.lin@gmail.com@a2882034-e4c0-11de-85bf-c3887d924cc8>
2010-02-24 08:15:46 +0000

    move two methods from class "common" to "plurk_api", re-define the config file, and error handling.

--- trunk/common.php
+++ trunk/common.php
@@ -16,31 +16,6 @@ Class common {
 
 	function __deconstruct() {}
 
-
-	/**
-	 * function get_permalink
-	 * transfer plurk_id to permalink
-	 *
-	 * @param $plurk_id
-	 * @return string.
-	 */
-	function get_permalink($plurk_id)
-	{
-		return "http://www.plurk.com/p/" . base_convert($plurk_id, 10, 36);
-	}
-
-	/**
-	 * function get_plurk_id
-	 * transfer permalink to plurk_id
-	 *
-	 * @param $permalink
-	 * @return int.
-	 */
-	function get_plurk_id($permalink)
-	{
-		return base_convert(str_replace('http://www.plurk.com/p/', '', $permalink), 36, 10);
-	}
-
 	/**
 	 * funciton log
 	 * message log
@@ -49,10 +24,7 @@ Class common {
 	 */
 	function log($message = '')
 	{
-		if(!file_exists(PLURK_LOG_PATH)) 
-		{
-			touch(PLURK_LOG_PATH);
-		}	
+		if( ! file_exists(PLURK_LOG_PATH)) 	touch(PLURK_LOG_PATH);
 		$source = file_get_contents(PLURK_LOG_PATH);
 		$source .= date("Y-m-d H:i:s - ") . $message . "\n";
 		file_put_contents(PLURK_LOG_PATH, $source);
